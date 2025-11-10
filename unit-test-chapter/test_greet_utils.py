14ef46e90b966405cc882b3eb7869e02986bcabc
Anurag Gupta <62739090+ag999git@users.noreply.github.com>
2025-11-10 16:16:14 +0530

    Update test_greet_utils.py

--- unit-test-chapter/test_greet_utils.py
+++ unit-test-chapter/test_greet_utils.py
@@ -2,18 +2,17 @@
 # Greeting Function (with a subtle bug, a hidden logic mistake). 
 # In the book the following problem was given:- The problem is is triple Quotes below
 
-"""
-PROBLEM
+""" PROBLEM STARTS HERE
 # greet_utils.py
 
 # Bug:- an empty string "" or a None value?
 def make_greeting(name):
     # BUG: forgets to handle empty or None names
     return "Hello, " + name + "!"
-"""
+""" PROBLEM ENDS HERE
 # The project was to create a unit test for the above problem
 
-# The solution is as follows:-
+# SOLUTION STARTS HERE
 
 # SOLUTION
 # test_temperature_utils.py
@@ -26,3 +25,5 @@ class TestTemperatureConversion(unittest.TestCase):
         self.assertEqual(result, 212)  # 100°C should be 212°F
 
 unittest.main()
+
+# SOLUTION ENDS HERE
